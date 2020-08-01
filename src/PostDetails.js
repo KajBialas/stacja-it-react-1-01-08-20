29b5990ec1db5ad7df5561959e3815e19d451fa5
Kaj Białas <kaj.bialas@dev-industry.com>
2020-08-01 14:01:35 +0200

    Changes with issue

--- src/PostDetails.js
+++ src/PostDetails.js
@@ -1,29 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class PostDetails extends Component {
   state = {
     postDetails: {},
     isLoading: false,
+    isError: false,
   };
 
   componentDidMount() {
+    this.setState({
+      isLoading: true,
+      isError: false,
+    });
+
     fetch('https://jsonplaceholder.typicode.com/posts/1')
       .then(response => response.json())
-      .then(data => this.setState({
+      .then(data => setTimeout(() => this.setState({
         postDetails: data,
+        isLoading: false,
+      }), 3000))
+      .catch(e => this.setState({
+        isLoading: false,
+        isError: true,
       }));
   };
 
   render() {
+    const {isLoading, isError, postDetails: {title, body}} = this.state;
+
     return (
       <div>
         <h2>Szczegóły postu:</h2>
-        <h3>
-          {this.state.postDetails.title}
-        </h3>
-        <p>
-          {this.state.postDetails.body}
-        </p>
+        {isLoading && <div>Loading...</div>}
+
+        {title && body && <>
+          <h3>
+            {this.state.postDetails.title}
+          </h3>
+          <p>
+            {this.state.postDetails.body}
+          </p>
+        </>}
+
+        {isError && <div>Błąd pobierania danych</div>}
       </div>
     )
   }
